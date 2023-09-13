1c127601e8144f91f31983aa404dbcbff1f22889
Emily Butler <ekb21a@fsu.edu>
2023-09-13 11:30:58 -0400

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+let x = 200;
+let speed = 5;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(125);
+  background("pink");
+  ellipse(x,height/2,40);
+  if (x >= width - diameter/2)
+  speed = speed * -1;
+  x = x + speed;
 }
